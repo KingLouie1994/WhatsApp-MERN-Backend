b8f60a86ea29a3acb817dd5c545af05115896b2e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-04 16:20:38 +0100

    route to get all messages

--- routes/messages.js
+++ routes/messages.js
@@ -4,6 +4,17 @@ const router = require("express").Router();
 // Import schema
 const Message = require("../models/message");
 
+// Route to get all messages
+router.get("/all", (req, res, next) => {
+  Message.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
 // Route to create new message
 router.post("/new", (req, res, next) => {
   const message = req.body;
