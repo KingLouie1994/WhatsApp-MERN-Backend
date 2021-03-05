21a7871da8a6f6e9afa0579bd5ea5df6ad297add
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-05 10:45:23 +0100

    minor error fixed

--- server.js
+++ server.js
@@ -72,6 +72,9 @@ db.once("open", () => {
       pusher.trigger("messages", "inserted", {
         name: messageDetails.name,
         message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+        _id: messageDetails._id,
       });
     } else {
       console.log("Error triggering Pusher");
